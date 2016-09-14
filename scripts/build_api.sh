9b6219a99aa7c525baa601eb62c034161c78f3fe
btravers <benoit.travers.fr@gmail.com>
2016-09-14 21:51:37 +0200

    Scripts

--- scripts/build_api.sh
+++ scripts/build_api.sh
@@ -3,10 +3,20 @@
 API_PATH=${SCRIPT_PATH}/api
 
 echo -e "${OUTPUT_COLOR}Building api image${NC}"
+docker build \
+  --build-arg USER_ID=$(id -u) \
+  -t abr/maven \
+  "${SCRIPTS_DIR}/maven"
+
 docker run --rm \
-   -v ~/.m2:/root/.m2 \
+   -v ~/.m2:/home/username/.m2 \
    -v "${API_PATH}":/usr/src/app \
    -v /var/run/docker.sock:/var/run/docker.sock \
    -w /usr/src/app \
-   maven:3.3.9-jdk-8 \
-   mvn clean package docker:build -DskipTests
+   abr/maven \
+   mvn clean package -DskipTests
+
+docker build \
+  -t abr/api \
+  -f "${API_PATH}/docker/Dockerfile" \
+  "${API_PATH}"
--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -3,18 +3,26 @@
 UI_PATH=${SCRIPT_PATH}/ui
 
 echo -e "${OUTPUT_COLOR}Building ui image${NC}"
+docker build \
+  --build-arg USER_ID=$(id -u) \
+  -t abr/node \
+  "${SCRIPTS_DIR}/node"
+
 docker run --rm \
   -v "${UI_PATH}":/usr/src/app/ \
   -e NODE_ENV=development \
   -w /usr/src/app/ \
-  node:6.5.0 \
+  abr/node \
   npm install
 
 docker run --rm \
   -v "${UI_PATH}":/usr/src/app/ \
   -e NODE_ENV=production \
   -w /usr/src/app/ \
-  node:6.5.0 \
+  abr/node \
   npm run build
 
-docker build -f "${UI_PATH}/docker/Dockerfile" -t abr/ui "${UI_PATH}"
+docker build \
+  -f "${UI_PATH}"/docker/Dockerfile \
+  -t abr/ui \
+  "${UI_PATH}"
